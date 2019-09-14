e55041710f38d1d55e289d4f5af3d2c3281ac56f
Hakeem Almidan <hakeemmidan@gmail.com>
2019-09-14 14:05:51 -0700

    Make products viewable in cart

--- frontend/components/carts/show/cart_show.jsx
+++ frontend/components/carts/show/cart_show.jsx
@@ -1,19 +1,51 @@
 import React from 'react';
+import { ProductIndexItem } from '../../products/index/product_index_item';
 
 export class CartShow extends React.Component {
-    componentDidMount() {
-        this.props.fetchCart(this.props.match.params.cartId)
-    }
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.match.params.cartId != this.props.match.params.cartId) {
-            this.props.fetchCart(this.props.match.params.cartId)
+    constructor(props) {
+        super(props)
+        this.state = {
+            cartProducts: []
         }
     }
 
+    componentDidMount() {
+        // this.props.fetchCart(this.props.match.params.cartId)
+        JSON.parse(localStorage.cart).map( productId => (
+            this.props.fetchProduct(productId)
+                .then(response => this.setState(state => {
+                    const cartProducts = state.cartProducts.concat(response.product)
+                    return {
+                        cartProducts
+                    }
+                })
+        )))
+    }
+
     render() {
+        if (this.state.cartProducts.length < JSON.parse(localStorage.cart).length) {
+            return (
+                <div>
+                    <div>Loading...</div>
+                </div>
+            )
+        }
+
+        const cartProductsLis = this.state.cartProducts.map( (product, idx) => {
+            return (
+                <li>
+                    <ProductIndexItem
+                        key={`cart-item-${idx}`}
+                        product={product}
+                    />
+                </li>
+            )
+        })
+
         return (
-            <div>I am the cart!!!!!!</div>
+            <ul>
+                {cartProductsLis}
+            </ul>
         )
     }
 }
--- frontend/components/carts/show/cart_show_container.jsx
+++ frontend/components/carts/show/cart_show_container.jsx
@@ -2,18 +2,20 @@ import { connect } from 'react-redux';
 
 import { fetchCart } from '../../../actions/cart_actions';
 import { CartShow } from './cart_show';
+import { fetchProduct } from '../../../actions/product_actions';
 
 const mapStateToProps = (state, ownProps) => {
     // question ) What are ownProps and where do they come from?
     return {
         cart: state.entities.carts[ownProps.match.params.cartId],
-        cartId : ownProps.match.params.cartId
+        cartId : ownProps.match.params.cartId,
     }
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        fetchCart: (id) => dispatch(fetchCart(id))
+        fetchCart: (id) => dispatch(fetchCart(id)),
+        fetchProduct: (id) => dispatch(fetchProduct(id))
     }
 }
 
