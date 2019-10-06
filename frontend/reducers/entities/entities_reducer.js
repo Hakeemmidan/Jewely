a9fee83ad2a90f51efd0085e32321432fab09b0e
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-06 10:13:06 -0700

    Add reviewsReducer to entitesReducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { usersReducer } from './users_reducer';
 import { productsReducer } from './products_reducer';
 import { cartsReducer } from './carts_reducer';
+import { reviewsReducer } from './reviews_reducer';
 
 export const entitiesReducer = combineReducers({
   users: usersReducer,
   products: productsReducer,
-  carts: cartsReducer
+  carts: cartsReducer,
+  reviews: reviewsReducer
 });
