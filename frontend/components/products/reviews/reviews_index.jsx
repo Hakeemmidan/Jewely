f983b4103f91c0f61671b6b1c3626b6f0f08f1d3
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-06 12:01:38 -0700

    Create reviews container

---
+++ frontend/components/products/reviews/reviews_index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export class ReviewsIndex extends Component {
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                hello I am the reviews index   
+            </div>
+        )
+    }
+}
