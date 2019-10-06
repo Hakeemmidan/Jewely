0b8841890519aab2e78d181daf33235348cc0bf4
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-06 12:06:08 -0700

    Create skeleton for ReviewsIndex

--- frontend/components/products/reviews/reviews_index.jsx
+++ frontend/components/products/reviews/reviews_index.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
 export class ReviewsIndex extends Component {
+    constructor(props) {
+        super(props)
+
+        this.renderReviews = this.renderReviews.bind(this)
+    }
+
     componentDidMount() {
-        
+        this.props.fetchReviews()
+    }
+
+    renderReviews() {
+        this.props.reviews.map(review => {
+            <li>
+                <h4>
+                    {review.title}
+                </h4>
+                <p>
+                    {review.body}
+                </p>
+                <p>
+                    {review.rating}
+                </p>
+            </li>
+        })
     }
 
     render() {
         return (
             <div>
-                hello I am the reviews index   
+                <ul>
+                    {this.renderReviews()}
+                </ul>
             </div>
         )
     }
