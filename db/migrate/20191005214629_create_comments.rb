800dd976d9ab988a7284335d6e78df0fe2186abe
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-09 21:04:40 -0700

    Fix migration spelling error

--- db/migrate/20191005214629_create_comments.rb
+++ db/migrate/20191005214629_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :title, null: false
       t.text :body, null: false
-      t.decimal :price, null: flase
+      t.decimal :price, null: false
       t.timestamps
     end
   end
