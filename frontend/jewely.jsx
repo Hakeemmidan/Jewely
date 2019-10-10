7d0ecafe989732282c47caa51fefdec584867414
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-09 21:01:27 -0700

    Remove window testing item

--- frontend/jewely.jsx
+++ frontend/jewely.jsx
@@ -26,8 +26,4 @@ document.addEventListener("DOMContentLoaded", () => {
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
-
-  // TESTING START
-    window.getState = store.getState;
-  // TESTING END
 });
