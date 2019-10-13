e32f7b4de4190142d66572c299e94fe97d010426
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-13 12:19:42 -0700

    Create modal component

--- frontend/components/products/reviews/modal.jsx
+++ frontend/components/products/reviews/modal.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import CreateReviewContainer from './create_review_form_container';
+import EditReviewCOntainer from './edit_review_form_container';
 
-export function Modal({modal, closeModal}) {    
+export function Modal({modal, closeModal}) {
+    // If modal not in global state, don't display
+    if (!modal) {
+        return null
+    }
+
+    let component;
+    switch (modal) {
+        case 'create review':
+            component = <CreateReviewContainer />            
+            break;
+        case 'edit review':
+            component = <EditReviewCOntainer />
+            break;
+        default:
+            return null;
+    }
+    
     return (
         <div>
-            This is the modal!
+            <div className="modal-background" onClick={closeModal}>
+                <div className="modal-child" onClick={e => e.stopPropagation()}>
+                    { component }
+                </div>
+            </div>
         </div>
     )
 }
