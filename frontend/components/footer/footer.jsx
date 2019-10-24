ac4a501ff487ce71f76e42b4be45fac407948101
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-23 19:00:13 -0700

    Change footer into functional component

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 
-export class Footer extends React.Component {
-    render() {
-        return (
-            <div class="personal-accounts-links">
-                <a
-                    href="https://github.com/Hakeemmidan"
-                    target="_blank">
-                    <img
-                        class="personal-account-img-link"
-                        src="https://image.flaticon.com/icons/svg/733/733609.svg"
-                        alt="Github logo"/>
-                </a>
+export function Footer() {
+    return (
+        <div class="personal-accounts-links">
+            <a
+                href="https://github.com/Hakeemmidan"
+                target="_blank">
+                <img
+                    class="personal-account-img-link"
+                    src="https://image.flaticon.com/icons/svg/733/733609.svg"
+                    alt="Github logo"/>
+            </a>
 
-                <a
-                    href="https://www.linkedin.com/in/abdulhakeem-almidan/"
-                    target="_blank">
-                    <img
-                        class="personal-account-img-link"
-                        src="https://image.flaticon.com/icons/svg/25/25320.svg"
-                        alt="LinkedIn logo"/>
-                </a>
-            </div>
-        )
-    }
+            <a
+                href="https://www.linkedin.com/in/abdulhakeem-almidan/"
+                target="_blank">
+                <img
+                    class="personal-account-img-link"
+                    src="https://image.flaticon.com/icons/svg/25/25320.svg"
+                    alt="LinkedIn logo"/>
+            </a>
+        </div>
+    )
 }
