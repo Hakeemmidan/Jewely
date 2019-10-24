064ccf522418e069975b8e99cfeda2bc13123594
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-24 10:33:36 -0700

    Get personal accounts to show onClick on 'proceed to checkout'

--- frontend/components/carts/show/cart_proceed_to_checkout.jsx
+++ frontend/components/carts/show/cart_proceed_to_checkout.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 export function CartProceedToCheckout() {
     return (
-        <div className="proceed-to-checkout-modal-div">
-            Hello world my name is Hakeem
+        <div className="proceed-to-checkout-modal-div checkout">
+                <a
+                    href="https://github.com/Hakeemmidan"
+                    target="_blank">
+                    <img
+                        className="personal-account-img-link checkout"
+                        src="https://image.flaticon.com/icons/svg/733/733609.svg"
+                        alt="Github logo" />
+                </a>
+                &nbsp;
+                <a
+                    href="https://www.linkedin.com/in/abdulhakeem-almidan/"
+                    target="_blank">
+                    <img
+                        className="personal-account-img-link checkout linkedin"
+                        src="https://image.flaticon.com/icons/svg/25/25320.svg"
+                        alt="LinkedIn logo" />
+                </a>
         </div>
     )
 }
