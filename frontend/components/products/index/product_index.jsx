0a5fdec3cf497856465fa399a7e89611449b3a2a
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-24 11:16:01 -0700

    Have home image that has text on it

--- frontend/components/products/index/product_index.jsx
+++ frontend/components/products/index/product_index.jsx
@@ -15,6 +15,15 @@ export class ProductIndex extends React.Component {
                 <h2 className="product_index_catchphrase">
                     If it's shiny, custom, or fancy, it's on Jewely
                 </h2>
+                <div className="product-index-home-image-container">
+                    <img
+                        className="product-index-home-image" 
+                        src="https://images.unsplash.com/photo-1508801283163-8e6e3090b523?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80">
+                    </img>
+                    <div className="product-index-home-image-text">
+                        Jewelry shines brighter <br/> when it's custom
+                    </div>
+                </div>
 
                 <div className="product-index-item-list-container">
                     {/* <Carousel className="product-index-photo-carousel"/> */}
