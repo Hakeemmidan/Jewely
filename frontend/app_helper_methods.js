3ba1bc5a4183a3ac2b8ca14141f2c30194a8eaf2
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-25 16:37:47 -0700

    Create convertArrayOfObjectsToObject() function

---
+++ frontend/app_helper_methods.js
@@ -0,0 +1,10 @@
+export const convertArrayOfObjectsToObject = (arrayOfObjects) => {
+    const resObj = {}
+
+    // The objects being passed must have an id property
+    arrayOfObjects.forEach(obj => {
+        resObj[obj.id] = obj
+    })
+
+    return resObj
+}
