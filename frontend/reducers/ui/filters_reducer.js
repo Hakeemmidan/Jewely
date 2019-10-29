c1fe2a612d42de61a1e72bba62cac7ff2952fac9
Hakeem Almidan <hakeemmidan@gmail.com>
2019-10-29 13:23:31 -0700

    Create filters reducer

---
+++ frontend/reducers/ui/filters_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_UPDATE_FILTER } from '../../actions/filter_actions';
+
+export const filtersReducer = (oldState = {search: ''}, action) => {
+    Object.freeze(oldState);
+    if (action.type === RECEIVE_UPDATE_FILTER) {
+        const newFilter = {
+            [action.filter]: action.value
+        };
+        return merge({}, oldState, newFilter);
+    } else {
+        return oldState;
+    }
+};
