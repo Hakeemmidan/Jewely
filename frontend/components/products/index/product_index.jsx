d63e46ea11916b8aa87255ab8e7a3878bff8c964
Hakeem Almidan <hakeemmidan@gmail.com>
2021-01-19 00:13:20 +0300

    Fix react-router go-back page crash

--- frontend/components/products/index/product_index.jsx
+++ frontend/components/products/index/product_index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ScrollMenu from 'react-horizontal-scrolling-menu';
+import {Link} from 'react-router-dom';
 import {ProductIndexItem} from './product_index_item';
 
 export class ProductIndex extends React.Component {
@@ -15,10 +16,15 @@ export class ProductIndex extends React.Component {
           arrowLeft={<div className="scroll-menu-arrow">{' < '}</div>}
           arrowRight={<div className="scroll-menu-arrow">{' > '}</div>}
           data={this.props.products.map((product, idx) => (
-            <ProductIndexItem
-              key={`product-index-item-idx-${idx}`}
-              product={product}
-            />
+            <Link
+              to={`/products/${product.id}`}
+              style={{textDecoration: 'none'}}
+            >
+              <ProductIndexItem
+                key={`product-index-item-idx-${idx}`}
+                product={product}
+              />
+            </Link>
           ))}
         />
       </div>
--- frontend/components/products/index/product_index_item.jsx
+++ frontend/components/products/index/product_index_item.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 export function ProductIndexItem(props) {
   const product = props.product;
 
   return (
     <div key={product.id} className="product-index-item">
-      <Link to={`/products/${product.id}`} style={{textDecoration: 'none'}}>
-        <img
-          src={product.photoUrls[0]}
-          className="product-index-image"
-          alt={product.title}
-          title={product.title}
-        />
-        <p className="product-index-item-title">{product.title}</p>
-        <p className="product-index-item-seller-username">
-          {product.seller_username}
-        </p>
-        <p className="product-index-item-price">
-          $
-          {parseFloat(product.price)
-            .toFixed(2)
-            .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
-        </p>
-        {/* Number format source : https://stackoverflow.com/a/14428340/7974948  */}
-      </Link>
+      <img
+        src={product.photoUrls[0]}
+        className="product-index-image"
+        alt={product.title}
+        title={product.title}
+      />
+      <p className="product-index-item-title">{product.title}</p>
+      <p className="product-index-item-seller-username">
+        {product.seller_username}
+      </p>
+      <p className="product-index-item-price">
+        $
+        {parseFloat(product.price)
+          .toFixed(2)
+          .replace(/\d(?=(\d{3})+\.)/g, '$&,')}
+      </p>
+      {/* Number format source : https://stackoverflow.com/a/14428340/7974948  */}
     </div>
   );
 }
